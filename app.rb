b6ccd683093965712884c92e69d77d3c04920f6a
Cody Poll <cpoll@instructure.com>
2015-09-18 20:19:52 -0600

    Fixes

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
+require 'slackoff'
 
 post '/guild-wars' do
   message = '@channel: Come play guild wars with me!'
   slack = Slackoff::Slack.new
   hook = slack.incoming_webhook(ENV['guild_wars_hook'])
   r = hook.send(text: message)
-  puts r.body
-  status r.status
+  status 200
   body ''
 end
