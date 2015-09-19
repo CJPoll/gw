138e62ad622ce4653dcbf9a9836b1bbb05b8f931
Cody Poll <cpoll@instructure.com>
2015-09-18 22:42:52 -0600

    Updated slackoff version

--- app.rb
+++ app.rb
@@ -1,11 +1,24 @@
+require 'rubygems'
 require 'sinatra'
 require 'slackoff'
+require 'slackoff/slash'
 
 post '/guild-wars' do
-  message = '@channel: Come play guild wars with me!'
+  slash_data = Slackoff::SlashData.new(params)
+
+  text = "@channel: Hey, it's #{slash_data.user_name}. Come play guild wars with me!"
+  username = 'Guild Wars 2 Notifications'
+  channel = 'testing-slashes'
+
+  message = Slackoff::Post.new
+  message.text = text
+  message.username = username
+  message.channel = channel
+
   slack = Slackoff::Slack.new
   hook = slack.incoming_webhook(ENV['guild_wars_hook'])
-  r = hook.send(text: message)
+  hook.send(message)
+
   status 200
   body ''
 end
