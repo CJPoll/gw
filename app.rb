3d5342973062336ffcb73a55733f977280d3042c
Cody Poll <cpoll@instructure.com>
2015-09-18 19:56:30 -0600

    Initial commit

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+
+get '/guild-wars' do
+  message = '@channel: Come play guild wars with me!'
+  slack = Slackoff::Slack.new
+  hook = slack.incoming_webhook(ENV['guild_wars_hook'])
+  hook.send(text: message)
+end
