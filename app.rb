db24be51f32edd3b432cbaa1bed7c0c6df9ff628
Cody Poll <cpoll@instructure.com>
2015-09-18 22:44:26 -0600

    Switched channel from test to #guild-wars

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ post '/guild-wars' do
 
   text = "@channel: Hey, it's #{slash_data.user_name}. Come play guild wars with me!"
   username = 'Guild Wars 2 Notifications'
-  channel = 'testing-slashes'
+  channel = 'guild-wars'
 
   message = Slackoff::Post.new
   message.text = text
