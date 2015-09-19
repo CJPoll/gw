238b0351d7dfa82bf2ca0d17a303806bcc72b38a
Cody Poll <cpoll@instructure.com>
2015-09-18 20:13:05 -0600

    Derp - not javascript

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 
 post '/guild-wars' do
   message = '@channel: Come play guild wars with me!'
-  console.log(ENV['guild_wars_hook'])
   slack = Slackoff::Slack.new
   hook = slack.incoming_webhook(ENV['guild_wars_hook'])
   r = hook.send(text: message)
